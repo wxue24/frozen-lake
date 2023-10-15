fa00b87a91c659a6ac5dc50aaee50aa291afb734
Will Xue <xuewilliam5@gmail.com>
2023-10-15 14:54:45 -0400

    extract policy and print

--- value_iteration_assignment_3.py
+++ value_iteration_assignment_3.py
@@ -14,7 +14,6 @@ actions = {0: "left", 1: "down", 2: "right", 3: "up"}
 
 initialValues = dict.fromkeys(range(16), 0)
 
-initialPolicy = dict.fromkeys(range(16), "")
 
 # Set all rewards to 0 except goal to equal 1
 initialRewards = dict.fromkeys(range(16), 0)
@@ -70,7 +69,6 @@ class Agent:
         self.rewards = initialRewards
         self.values = initialValues
         self.transitions = initialTransitions
-        self.policy = initialPolicy
         self.env = env
         self.currentReward = 0
         self.currentRow = 0
@@ -86,6 +84,7 @@ class Agent:
         pass
 
     def play_n_random_steps(self, count):
+        """Play until agent is terminated or maximize number of steps (count) has been taken"""
         for i in range(count):
             random_action = self.env.action_space.sample()
             obs, reward, terminated, truncated, info = self.env.step(random_action)
@@ -103,24 +102,56 @@ class Agent:
 
             if terminated:
                 if reward == 0:
-                    self.rewards[obs] = -1
+                    self.rewards[obs] = - 10
                 self.env.reset()
                 return
 
     def print_value_table(self):
-        printTableFromDict(self.values)
+        """Prints a table in the shape of a grid from a dictionary where the key is the state"""
+        for i in range(16):
+            self.values[i] = round(self.values[i], 2)
+        print(f'{self.values[0]:5} | {self.values[1]:5} | {self.values[2]:5} | {self.values[3]:5}')
+        print(f'{self.values[4]:5} | {self.values[5]:5} | {self.values[6]:5} | {self.values[7]:5}')
+        print(f'{self.values[8]:5} | {self.values[9]:5} | {self.values[10]:5} | {self.values[11]:5}')
+        print(f'{self.values[12]:5} | {self.values[13]:5} | {self.values[14]:5} | {self.values[15]:5}')
+        print("")
 
     def extract_policy(self):
-        pass
+        """Extracts policy at each state and returns as a dict"""
+        policy = dict.fromkeys(range(16), "")
+        for s in range(16):
+            action, value = self.select_action(s)
+            policy[s] = action
+        return policy
 
     def print_policy(self, policy):
-        printTableFromDict(policy)
+        """Prints policy as a table"""
+        print(f'{policy[0]:5} | {policy[1]:5} | {policy[2]:5} | {policy[3]:5}')
+        print(f'{policy[4]:5} | {policy[5]:5} | {policy[6]:5} | {policy[7]:5}')
+        print(f'{policy[8]:5} | {policy[9]:5} | {policy[10]:5} | {policy[11]:5}')
+        print(f'{policy[12]:5} | {policy[13]:5} | {policy[14]:5} | {policy[15]:5}')
+        print("")
 
     def calc_action_value(self, state, action):
         pass
 
     def select_action(self, state):
-        pass
+        "Returns tuple of best action and value"
+        actions_values= []
+        # determine which actions can be taken and append the action-value pair
+        for a in ["up", "down", "left", "right"]:
+            if (state, a) in self.transitions:
+                s_a = self.transitions[(state, a)]
+                actions_values.append((a, self.values[s_a]))
+        
+        bestAction = None
+        bestValue = 0
+        for action, value in actions_values:
+            if value > bestValue:
+                bestAction = action
+                bestValue = value
+        
+        return (bestAction, bestValue)
 
     def play_episode(self, env):
         pass
@@ -136,7 +167,7 @@ class Agent:
 
                 probability = 1 / len(actions_states)
 
-                q = -1
+                q = 0
                 for a, s_a in actions_states:
                     # Apply bellman equation
                     q = max(
@@ -156,7 +187,9 @@ if __name__ == "__main__":
         iter_no += 1
         agent.play_n_random_steps(100)
         agent.value_iteration()
-        agent.print_value_table()
+        # agent.print_value_table()
+        policy = agent.extract_policy()
+        agent.print_policy(policy)
 
         reward = agent.currentReward
         if reward > best_reward:
